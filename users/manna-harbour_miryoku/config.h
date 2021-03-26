989a510915b872b2002013aa9a1437df41b129bc
nova <nova@McMe.local>
2021-03-26 18:28:14 +0100

    Add personal settings

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -3,7 +3,10 @@
 #pragma once
 
 // default but used in macros
-#define TAPPING_TERM 200
+#define TAPPING_TERM 350
+
+// Recommended for higher TAPPING_TERM
+#define PERMISSIVE_HOLD
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
@@ -13,7 +16,7 @@
 
 // Auto Shift
 #define NO_AUTO_SHIFT_ALPHA
-#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_TIMEOUT 200
 #define AUTO_SHIFT_NO_SETUP
 
 // Recommended for heavy chording.
